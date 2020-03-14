9fab114c092a2124c1b11ed0202053500bb437c4
Sarabjot Sethi <J.A.R.V.I.S@Sarabjots-MacBook-Pro.local>
2020-03-15 00:39:13 +0530

    first commit

---
+++ src/main/java/com/sarab/sarab/SarabApplication.java
@@ -0,0 +1,13 @@
+package com.sarab.sarab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarab/sarab/SarabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarab.sarab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
